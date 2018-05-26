2afdcb2d0f2836d1acd927642def445b0a1a09d1
Antoine Quellier <antoinecquellier@gmail.com>
2018-05-27 00:35:05 +0100

    added javascript typped on homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'articles/index'
+  get 'articles/show'
+  get 'articles/new'
+  get 'articles/create'
+  get 'articles/edit'
+  get 'articles/update'
+  get 'articles/destroy'
   get 'challenges/index'
   get 'challenges/show'
   get 'challenges/new'
@@ -9,4 +16,5 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :challenges, except: [:destroy]
+  resources :articles
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_26_155716) do
+ActiveRecord::Schema.define(version: 2018_05_26_230136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.string "author"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "challenges", force: :cascade do |t|
     t.string "name"
