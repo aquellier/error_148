90ff0c139a591b4d3cd1fbab31f0a96553fe17f4
Antoine Quellier <antoinecquellier@gmail.com>
2018-05-28 09:50:39 +0100

    Deploy on heroku

--- Gemfile
+++ Gemfile
@@ -6,7 +6,7 @@ gem 'figaro'
 gem 'jbuilder', '~> 2.0'
 gem 'devise'
 
-gem 'pg', '~> 0.21'
+gem 'pg'
 gem 'puma'
 gem 'rails', '5.2.0'
 gem 'redis'
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable [:facebook, :linkedin, :github]
+         :recoverable, :rememberable, :trackable, :validatable
 
 
   has_many :challenges
